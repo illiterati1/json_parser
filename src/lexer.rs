01899ee951a5d37d14985f4a13c8528e6d3c48d9
Soiarse <brendan.x.wilson@gmail.com>
2024-11-06 18:57:14 -0600

    Splitting files and testing

---
+++ src/lexer.rs
@@ -0,0 +1,321 @@
+use anyhow::{bail, Result};
+
+#[derive(Debug, PartialEq)]
+pub enum Token<'a> {
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+    Colon,
+    Comma,
+    Str(&'a str),
+    Number(Numeric),
+    True,
+    False,
+    Null,
+}
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub enum Numeric {
+    Float(f64),
+    Integer(i64),
+}
+
+pub struct Lexer<'a> {
+    json: &'a str,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(json: &'a str) -> Self {
+        Self { 
+            json,
+        }
+    }
+
+    pub fn lex(&mut self) -> Result<Vec<Token>> {
+        use Token::*;
+        let mut tokens: Vec<Token> = vec![];
+        loop {
+            let mut chars = self.json.chars();
+            match chars.next() {
+                Some(c) => {
+                    match c {
+                        '{' => {
+                            tokens.push(LeftBrace);
+                            self.json = chars.as_str();
+                        }
+                        '}' => {
+                            tokens.push(RightBrace);
+                            self.json = chars.as_str();
+                        }
+                        '[' => {
+                            tokens.push(LeftBracket);
+                            self.json = chars.as_str();
+                        }
+                        ']' => {
+                            tokens.push(RightBracket);
+                            self.json = chars.as_str();
+                        }
+                        ':' => {
+                            tokens.push(Colon);
+                            self.json = chars.as_str();
+                        }
+                        ',' => {
+                            tokens.push(Comma);
+                            self.json = chars.as_str();
+                        }
+                        '"' => {
+                            let s = self.lex_string()?;
+                            tokens.push(Str(s))
+                        }
+                        '0'..='9' | '-' => {
+                            let num = self.lex_number()?;
+                            tokens.push(Number(num));
+                        }
+                        't' if self.json.starts_with("true") => {
+                            tokens.push(True);
+                            self.json = &self.json["true".len()..];
+                        }
+                        'f' if self.json.starts_with("false") => {
+                            tokens.push(False);
+                            self.json = &self.json["false".len()..];
+                        }
+                        'n' if self.json.starts_with("null") => {
+                            tokens.push(Null);
+                            self.json = &self.json["null".len()..];
+                        }
+                        ' ' | '\t' | '\r' | '\n' => {
+                            self.json = chars.as_str();
+                        }
+                        c => bail!("Lexing failure on character {c}"),
+                    }
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+        Ok(tokens)
+    }
+
+    fn lex_string(&mut self) -> Result<&'a str> {
+        let mut charindices = self.json.char_indices();
+        let _ = charindices.next(); // skip first '"'
+        let mut escape = false;
+        let n;
+        loop {
+            match charindices.next() {
+                None => bail!("Unexpected end of input in string literal"),
+                Some((i, '"')) if !escape => {
+                    n = i;
+                    break;
+                }
+                Some((_, '\\')) => {
+                    escape = true;
+                }
+                Some(_) => {
+                    escape = false;
+                }
+            }
+        }
+        let result = &self.json[1..n];
+        self.json = charindices.as_str();
+        Ok(result)
+    }
+
+    fn lex_number(&mut self) -> Result<Numeric> {
+        let mut charindices = self.json.char_indices().peekable();
+        let mut n = 0;
+        let mut floating_point = false;
+        if matches!(charindices.peek(), Some((_, '-'))) {
+            charindices.next();
+        }
+        if matches!(charindices.peek(), Some((_, '.')) | Some((_, 'e')) | Some((_, 'E'))) {
+            bail!("Invalid numeric literal");
+        }
+        if matches!(charindices.peek(), Some((_, '0'))) {
+            charindices.next().unwrap(); // safe from if condition
+            if let Some((_, c)) = charindices.peek() {
+                match c {
+                    'e' | 'E' | '.' | ' ' | '\t' | '\r' | '\n' | ',' | ']' | '}' => {}
+                    _ => bail!("Invalid numeric literal"),
+                }
+            }
+        } 
+        // read integer
+        loop {
+            match charindices.peek() {
+                None => {
+                    n = self.json.as_bytes().len();
+                    break;
+                }
+                Some((i, c)) => {
+                    match c {
+                        '0'..='9' => {
+                            charindices.next();
+                        }
+                        '.' | 'E' | 'e'  => {
+                            break;
+                        }
+                        ' ' | '\t' | '\r' | '\n' | ',' | ']' | '}' => {
+                            n = *i;
+                            break;
+                        }
+                        _ => bail!("Invalid numeric literal"),
+                    }
+                }
+            }
+        }
+        // read fraction
+        if matches!(charindices.peek(), Some((_, '.'))) {
+            floating_point = true;
+            charindices.next();
+            loop {
+                match charindices.peek() {
+                    None => {
+                        n = self.json.as_bytes().len();
+                        break;
+                    }
+                    Some((i, c)) => match c {
+                        '0'..='9' => {
+                            charindices.next();
+                        }
+                        'e' | 'E' => break,
+                        ' ' | '\t' | '\r' | '\n' | ',' | ']' | '}' => {
+                            n = *i;
+                            break;
+                        }
+                        _ => bail!("Invalid numeric literal"),
+                    }
+                }
+            }
+        }
+        // read exponent
+        if matches!(charindices.peek(), Some((_, 'e')) | Some((_, 'E'))) {
+            floating_point = true;
+            charindices.next();
+            if matches!(charindices.peek(), Some((_, '+')) | Some((_, '-'))) {
+                charindices.next();
+            }
+            loop {
+                match charindices.peek() {
+                    None => {
+                        n = self.json.as_bytes().len();
+                        break;
+                    }
+                    Some((i, c)) => match c {
+                        '0'..='9' => {
+                            charindices.next();
+                        }
+                        ' ' | '\t' | '\r' | '\n' | ',' | ']' | '}' => {
+                            n = *i;
+                            break;
+                        }
+                        _ => bail!("Invalid numeric literal"),
+                    }
+                }
+            }
+        }
+        let result = if floating_point {
+            let res = self.json[0..n].parse::<f64>()?;
+            Ok(Numeric::Float(res))
+        } else {
+            let res = self.json[0..n].parse::<i64>()?;
+            Ok(Numeric::Integer(res))
+        };
+        self.json = &self.json[n..];
+        result
+    }
+} 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lexing_string() {
+        let text = r#""test \"\b\n\r\" string""#;
+        let mut lexer = Lexer::new(text);
+        let result_vec = lexer.lex().unwrap();
+        let result = &result_vec[0];
+        assert_eq!(*result, Token::Str(r#"test \"\b\n\r\" string"#));
+    }
+
+    #[test]
+    fn test_lexing_integer() {
+        let text = "1234567890 -999 0";
+        let mut lexer = Lexer::new(text);
+        let result_vec = lexer.lex().unwrap();
+        assert_eq!(result_vec[0], Token::Number(Numeric::Integer(1234567890)));
+        assert_eq!(result_vec[1], Token::Number(Numeric::Integer(-999)));
+        assert_eq!(result_vec[2], Token::Number(Numeric::Integer(0)));
+    }
+
+    #[test]
+    fn test_lexing_fraction() {
+        let text = "0.0 0.999 -100.0";
+        let mut lexer = Lexer::new(text);
+        let result_vec = lexer.lex().unwrap();
+        assert_eq!(result_vec[0], Token::Number(Numeric::Float(0.0)));
+        assert_eq!(result_vec[1], Token::Number(Numeric::Float(0.999)));
+        assert_eq!(result_vec[2], Token::Number(Numeric::Float(-100.0)));
+    }
+
+    #[test]
+    fn test_lexing_exponent() {
+        let text = "1e0 -1.0e1 100.001e3";
+        let mut lexer = Lexer::new(text);
+        let result_vec = lexer.lex().unwrap();
+        assert_eq!(result_vec[0], Token::Number(Numeric::Float(1.0)));
+        assert_eq!(result_vec[1], Token::Number(Numeric::Float(-10.0)));
+        assert_eq!(result_vec[2], Token::Number(Numeric::Float(100001.0)));
+    }
+
+    #[test]
+    fn test_lexing_bad_integer() {
+        let text = "010000";
+        let mut lexer = Lexer::new(text);
+        let result = lexer.lex();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_lexing_bad_fractional() {
+        let text = ".001";
+        let mut lexer = Lexer::new(text);
+        let result = lexer.lex();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_lexing_bad_exponent() {
+        let text = "E001";
+        let mut lexer = Lexer::new(text);
+        let result = lexer.lex();
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_lexing_keywords() {
+        let text = "true false null";
+        let mut lexer = Lexer::new(text);
+        let result_vec = lexer.lex().unwrap();
+        assert_eq!(result_vec[0], Token::True);
+        assert_eq!(result_vec[1], Token::False);
+        assert_eq!(result_vec[2], Token::Null);
+    }
+
+    #[test]
+    fn test_lexing_array() {
+        let text = "[1, true, 4]";
+        let mut lexer = Lexer::new(text);
+        let result_vec = lexer.lex().unwrap();
+        assert_eq!(result_vec[0], Token::LeftBracket);
+        assert_eq!(result_vec[1], Token::Number(Numeric::Integer(1)));
+        assert_eq!(result_vec[2], Token::Comma);
+        assert_eq!(result_vec[3], Token::True);
+        assert_eq!(result_vec[4], Token::Comma);
+        assert_eq!(result_vec[5], Token::Number(Numeric::Integer(4)));
+        assert_eq!(result_vec[6], Token::RightBracket);
+    }
+}
---
+++ src/parser.rs
@@ -0,0 +1,231 @@
+use std::collections::HashMap;
+use std::iter::Peekable;
+use std::slice::Iter;
+use anyhow::{bail, Result};
+use crate::lexer::Token;
+
+#[derive(Debug)]
+pub enum JsonTree<'a> {
+    Object(HashMap<&'a str, JsonTree<'a>>),
+    Array(Vec<JsonTree<'a>>),
+    Atom(Token<'a>),
+}
+
+pub struct Parser { }
+
+impl<'a> Parser {
+    pub fn new() -> Self {
+        Self { }
+    }
+
+    pub fn parse(&'a self, vec: &'a [Token]) -> Result<Option<JsonTree>> {
+        self.value(&mut vec.iter().peekable())
+    }
+
+    fn value(&'a self, iter: &mut Peekable<Iter<Token<'a>>>) -> Result<Option<JsonTree>> {
+        match iter.peek() {
+            Some(Token::LeftBrace) => {
+                match self.object(iter) {
+                    Ok(object) => Ok(Some(object)),
+                    Err(e) => Err(e.context("In parsing of object")),
+                }
+            }
+            Some(Token::LeftBracket) => {
+                match self.array(iter) {
+                    Ok(array) => Ok(Some(array)),
+                    Err(e) => Err(e.context("In parsing of array"))
+                }
+            }
+            Some(Token::Str(s)) => {
+                iter.next();
+                Ok(Some(JsonTree::Atom(Token::Str(s))))
+            }
+            Some(Token::Number(n)) => {
+                iter.next();
+                Ok(Some(JsonTree::Atom(Token::Number(*n))))
+            }
+            Some(Token::True) => {
+                iter.next();
+                Ok(Some(JsonTree::Atom(Token::True)))
+            }
+            Some(Token::False) => {
+                iter.next();
+                Ok(Some(JsonTree::Atom(Token::False)))
+            }
+            Some(Token::Null) => {
+                iter.next();
+                Ok(Some(JsonTree::Atom(Token::Null)))
+            }
+            None => Ok(None),
+            Some(t) => bail!("Unexpected token {t:?}"),
+        }
+    }
+
+    fn object(&'a self, iter: &mut Peekable<Iter<Token<'a>>>) -> Result<JsonTree> {
+        iter.next(); // consume left brace
+        let mut elements: HashMap<&str, JsonTree> = HashMap::new();
+        if matches!(iter.peek(), Some(Token::RightBrace)) {
+            return Ok(JsonTree::Object(elements));
+        }
+        loop {
+            let name = match iter.next() {
+                Some(Token::Str(s)) => s,
+                t => bail!("Invalid token {t:?}"),
+            };
+            match iter.next() {
+                Some(Token::Colon) => {}
+                Some(t) => bail!("Expected ':' but found {:?}", t),
+                None => bail!("Unexpected end of input"),
+            }
+            let member = match self.value(iter) {
+                Ok(Some(value)) => value,
+                Ok(None) => bail!("Unexpected end of input"),
+                Err(e) => return Err(e.context("In object member definition")),
+            };
+            elements.insert(name, member);
+            if matches!(iter.peek(), Some(Token::Comma)) {
+                iter.next();
+            } else {
+                break;
+            }
+        }
+        let token = iter.next();
+        if !matches!(token, Some(Token::RightBrace)) {
+            bail!("Expected }}, got {token:?}");
+        }
+        Ok(JsonTree::Object(elements))
+    }
+
+    fn array(&'a self, iter: &mut Peekable<Iter<Token<'a>>>) -> Result<JsonTree> {
+        iter.next();
+        let mut elements = Vec::new();
+        if matches!(iter.peek(), Some(Token::RightBracket)) {
+            iter.next();
+            return Ok(JsonTree::Array(elements));
+        }
+        loop {
+            let element = match self.value(iter) {
+                Ok(Some(value)) => value,
+                Ok(None) => bail!("Unexpected end of input"),
+                Err(e) => return Err(e.context("In array definition")),
+            };
+            elements.push(element);
+            if matches!(iter.peek(), Some(Token::Comma)) {
+                iter.next();
+            } else {
+                break;
+            }
+        }
+        let token = iter.next();
+        if !matches!(token, Some(Token::RightBracket)) {
+            bail!("Expected ] but got {token:?}");
+        }
+        Ok(JsonTree::Array(elements))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use core::panic;
+
+    use super::*;
+    use crate::{lexer::Numeric, Lexer};
+
+    #[test]
+    fn test_parse_empty_object() {
+        let contents = "{}";
+        let mut lexer = Lexer::new(contents);
+        let tokens = lexer.lex().unwrap();
+        let parser = Parser::new();
+        assert!(parser.parse(&tokens).is_ok());
+    }
+
+    #[test]
+    fn test_parse_invalid_object() {
+        let contents = r#"{"foo": }"#;
+        let mut lexer = Lexer::new(contents);
+        let tokens = lexer.lex().unwrap();
+        let parser = Parser::new();
+        assert!(parser.parse(&tokens).is_err());
+    }
+
+    #[test]
+    fn test_parse_basic_object1() {
+        let contents = r#"{"hello": "world"}"#;
+        let mut lexer = Lexer::new(contents);
+        let tokens = lexer.lex().unwrap();
+        let parser = Parser::new();
+        let jsontree = parser.parse(&tokens).unwrap().unwrap();
+        assert!(matches!(jsontree, JsonTree::Object(_)));
+        match jsontree {
+            JsonTree::Object(map) => 
+            assert!(matches!(map.get("hello").unwrap(), JsonTree::Atom(Token::Str("world")))),
+            _ => panic!(),
+        }
+    }
+
+    #[test]
+    fn test_parse_basic_object2() {
+        let contents = r#"{"hello": "world", "goodbye": true}"#;
+        let mut lexer = Lexer::new(contents);
+        let tokens = lexer.lex().unwrap();
+        let parser = Parser::new();
+        let jsontree = parser.parse(&tokens).unwrap().unwrap();
+        assert!(matches!(jsontree, JsonTree::Object(_)));
+        match jsontree {
+            JsonTree::Object(map) => {
+                assert!(matches!(map.get("hello").unwrap(), JsonTree::Atom(Token::Str("world"))));
+                assert!(matches!(map.get("goodbye").unwrap(), JsonTree::Atom(Token::True)));
+            }
+            _ => panic!(),
+        }
+    }
+
+    #[test]
+    fn test_parse_array() {
+        let contents = r#"[1, 2.0, "foo", false, null]"#;
+        let mut lexer = Lexer::new(contents);
+        let tokens = lexer.lex().unwrap();
+        let parser = Parser::new();
+        let jsontree = parser.parse(&tokens).unwrap().unwrap();
+        match jsontree {
+            JsonTree::Array(vec) => {
+                assert!(matches!(vec[0], JsonTree::Atom(Token::Number(Numeric::Integer(1)))));
+                assert!(matches!(vec[1], JsonTree::Atom(Token::Number(Numeric::Float(2.0)))));
+                assert!(matches!(vec[2], JsonTree::Atom(Token::Str("foo"))));
+                assert!(matches!(vec[3], JsonTree::Atom(Token::False)));
+                assert!(matches!(vec[4], JsonTree::Atom(Token::Null)));
+            }
+            _ => panic!(),
+        }
+    }
+
+    #[test]
+    fn test_parse_nested_array() {
+        let contents = r#"[[[]]]"#;
+        let mut lexer = Lexer::new(contents);
+        let tokens = lexer.lex().unwrap();
+        let parser = Parser::new();
+        let jsontree = parser.parse(&tokens).unwrap().unwrap();
+        match jsontree {
+            JsonTree::Array(level2) => {
+                match &level2[0] {
+                    JsonTree::Array(level3) => {
+                        assert!(matches!(level3[0], JsonTree::Array(_)))
+                    }
+                    _ => panic!(),
+                }
+            }
+            _ => panic!(),
+        }
+    }
+
+    #[test]
+    fn test_parse_complex_object() {
+        let contents = r#"{"hello": 99, "bar": [1, 2, 3, 4]}"#;;
+        let mut lexer = Lexer::new(contents);
+        let tokens = lexer.lex().unwrap();
+        let parser = Parser::new();
+        let jsontree = parser.parse(&tokens).unwrap().unwrap();
+    }
+}
